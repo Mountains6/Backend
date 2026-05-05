e2554b547717d4ce44cc6a5c7251c9f48db08303
YanaHeuer <YanaHeuer9@gmail.com>
2026-05-05 18:08:33 +0200

    add project

---
+++ lesson01/console-log-app/index.js
@@ -0,0 +1,8 @@
+console.log("HI");
+
+
+// docker build -t console-log-app .
+// docker run --rm console-log-app
+// docker tag console-log-app mountains6/console-log-app
+// docker push mountains6/console-log-app
+// docker pull katsiarynazavertyaeva/console-log-app:latest
