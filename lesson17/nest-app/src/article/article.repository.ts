9e5ff103a61311cee5e1af245454c59aaf6592f0
YanaHeuer <YanaHeuer9@gmail.com>
2026-06-05 08:04:36 +0200

    add Lesson17

---
+++ lesson17/nest-app/src/article/article.repository.ts
@@ -0,0 +1,23 @@
+// In Memory repository, no real DB
+import { Injectable } from '@nestjs/common';
+import { Article } from './article.interface';
+
+@Injectable()
+
+export class ArticleRepository {
+  private articles: Article[] = [
+    {
+      id: 0,
+      title: 'Optimisation in React',
+      content: 'useCallback, useMemo, memo',
+      author: 'Dan Abramov',
+      createdAt: new Date(),
+    },
+  ];
+
+  private idCounter = 1;
+
+  findAll(): Article[] {
+    return this.articles;
+  }
+}
